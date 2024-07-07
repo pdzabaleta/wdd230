b149cf750064c9d209ed80eec653c537a388a088
pdzabaleta <pdzabaleta@gmail.com>
2024-07-07 01:11:11 -0400

    w03

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,14 +1,31 @@
 
-for (let i = 1; i <= 365; i++) {
-    let element = document.querySelector(`[id^='fc-dom-${i}']`);
-    if (element) {
-        element.style.color = 'blue'; // Cambia 'blue' por el color deseado
+document.addEventListener('DOMContentLoaded', () => {
+    const visitMessage = document.getElementById('visitMessage');
+    const lastVisitKey = 'lastVisit';
+    const lastVisit = localStorage.getItem(lastVisitKey);
+    const currentVisit = new Date();
+  
+    if (!lastVisit) {
+        visitMessage.textContent = "Welcome! Let us know if you have any questions.";
+    } else {
+        const lastVisitDate = new Date(lastVisit);
+        const timeDifference = currentVisit - lastVisitDate;
+        const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  
+        if (timeDifference < 24 * 60 * 60 * 1000) {
+            visitMessage.textContent = "Back so soon! Awesome!";
+        } else if (daysDifference === 1) {
+            visitMessage.textContent = `You last visited 1 day ago.`;
+        } else {
+            visitMessage.textContent = `You last visited ${daysDifference} days ago.`;
+        }
     }
-}
-
-
-
-
+  
+    localStorage.setItem(lastVisitKey, currentVisit.toISOString());
+  });
+  
+  
+  
 const ctx = document.getElementById('myChart');
 
   new Chart(ctx, {
@@ -52,29 +69,3 @@ const ctx = document.getElementById('myChart');
 
     calendar.render();
 });
-document.addEventListener('DOMContentLoaded', () => {
-    const visitMessage = document.getElementById('visitMessage');
-    const lastVisitKey = 'lastVisit';
-    const lastVisit = localStorage.getItem(lastVisitKey);
-    const currentVisit = new Date();
-  
-    if (!lastVisit) {
-        visitMessage.textContent = "Welcome! Let us know if you have any questions.";
-    } else {
-        const lastVisitDate = new Date(lastVisit);
-        const timeDifference = currentVisit - lastVisitDate;
-        const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-  
-        if (timeDifference < 24 * 60 * 60 * 1000) {
-            visitMessage.textContent = "Back so soon! Awesome!";
-        } else if (daysDifference === 1) {
-            visitMessage.textContent = `You last visited 1 day ago.`;
-        } else {
-            visitMessage.textContent = `You last visited ${daysDifference} days ago.`;
-        }
-    }
-  
-    localStorage.setItem(lastVisitKey, currentVisit.toISOString());
-  });
-  
-  
